f2949d8514e9643d7920ff705af26e13da7fa773
MarcGuiot <marc.guiot@free.fr>
2020-06-25 14:10:56 +0200

    Add Json on GlobTypeSet Add addColumn on sql table. Fix http.register on same url

--- src/main/java/org/globsframework/xml/custom/AllAnnotations.java
+++ src/main/java/org/globsframework/xml/custom/AllAnnotations.java
@@ -4,5 +4,5 @@ import org.globsframework.metamodel.GlobModel;
 import org.globsframework.metamodel.impl.DefaultGlobModel;
 
 public class AllAnnotations {
-    public static GlobModel globModel = new DefaultGlobModel(_XmlAsNode.TYPE, _XmlValueAsCData.TYPE);
+    public static GlobModel MODEL = new DefaultGlobModel(_XmlAsNode.TYPE, _XmlValueAsCData.TYPE);
 }
